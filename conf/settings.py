31f26d1b0d017b1620c30a92b32337e8af614b00
didi123412 <hyeonjikim989@gmail.com>
2021-12-09 10:42:08 +0900

    allow_all & run_django

--- conf/settings.py
+++ conf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7xibhmvlx4$a3m4hfusvnu!&k)13z9y&%%1v5alos=-y*dyt!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
